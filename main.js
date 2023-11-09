94d50e1d0b8660485a2140a1136a2739b422af4b
DevSerine <serinovaboussaid@gmail.com>
2023-11-09 10:13:03 -0800

    deploy

---
+++ main.js
@@ -0,0 +1,11 @@
+var typed = new Typed(".text", {
+  strings: [
+    " Senior Technician On",
+    " Full-Stack Developer",
+    " CyberSecurity Administrator",
+  ],
+  typeSpeed: 100,
+  backSpeed: 50,
+  backDelay: 1000,
+  loop: true,
+});
